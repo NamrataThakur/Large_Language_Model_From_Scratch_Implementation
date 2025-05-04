07d5b47793d0c9208f91986c381f81b1d29ba796
NamrataThakur <namrata.thakur893@gmail.com>
2025-05-04 20:27:12 +0530

    GPT2 Pretraining Dataset creation code.

--- Dataloader/Pretraining/gpt2_pretrainDataset.py
+++ Dataloader/Pretraining/gpt2_pretrainDataset.py
@@ -27,19 +27,5 @@ class GPTCustomDataset(Dataset):
         return self.encoded_id[index], self.target_id[index]
     
 
-#Create the custom dataloader function that will call the GPTCustomDataset class to create the dataset from the given text:
-def GPTCustomDataloader(text, context_length=256, stride=128, batch_size=4,
-                        shuffle=True, drop_last=True,num_workers=0):
 
-    #Initializer the tokenizer
-    tokenizer = tiktoken.get_encoding("gpt2")
-
-    #Create the dataset with the tokenizer
-    custom_dataset = GPTCustomDataset(text,tokenizer, context_length, stride)
-
-    #Create the dataloader with the dataset
-    custom_dataloader = DataLoader(custom_dataset,batch_size=batch_size,
-                                   shuffle=shuffle, drop_last=drop_last, num_workers=num_workers)
-    
-    return custom_dataloader
     
