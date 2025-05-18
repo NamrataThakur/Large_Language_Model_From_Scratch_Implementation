d3171bf5e2e3b306d94746f441b529323c2d9458
NamrataThakur <namrata.thakur893@gmail.com>
2025-05-18 08:09:58 +0530

    Adding the dropout rate in the model config.

--- GPT_ClassificationFT/gpt2_model_config.py
+++ GPT_ClassificationFT/gpt2_model_config.py
@@ -18,10 +18,10 @@ class GPT2_ModelConfig:
                                 "gpt2_1558M" : {'embedding_dimension':1600, 'num_heads':25, 'num_layers':48},
                             }
 
-    def load_model_config(self, model_name):
+    def load_model_config(self, model_name, drop_rate = 0.0):
 
         updated_config = self.base_config.copy()
         updated_config.update(self.config_dict[model_name])
-        updated_config.update({'qkv_bias':True, 'context_length':1024})
+        updated_config.update({'qkv_bias':True, 'context_length':1024, 'dropout': drop_rate})
 
         return updated_config
