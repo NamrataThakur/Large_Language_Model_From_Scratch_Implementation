35f4610cd30ba14593c76049db1ab1605d512538
NamrataThakur <namrata.thakur893@gmail.com>
2025-05-22 12:02:40 +0530

    Instruction fine-tune dataset creation code.

---
+++ Dataloader/Instruction_finetuning/gpt2_instructDataset.py
@@ -0,0 +1,22 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+from gpt2_instructDataFormat import format_input_response
+
+class GPTCustomInstructionDataset(Dataset):
+    def __init__(self, input_data, tokenizer, prompt_style):
+        self.encoded_data = []
+        self.instruction_length = []
+        self.data = input_data
+
+        for row in input_data:
+
+            instruct_length, formatted_row = format_input_response(row, prompt_style)
+            tokenized_row = tokenizer.encode(formatted_row, allowed_special = 'all')
+            self.encoded_data.append(tokenized_row)
+            self.instruction_length.append(instruct_length)
+
+    def __getitem__(self, index):
+        return self.instruction_length[index], self.encoded_data[index]
+    
+    def __len__(self):
+        return len(self.data)
