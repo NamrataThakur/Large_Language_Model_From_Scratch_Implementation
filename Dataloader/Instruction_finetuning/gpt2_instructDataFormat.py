c5d3f934bcb39c1c329fa3fa65bf0014c0317c3b
NamrataThakur <namrata.thakur893@gmail.com>
2025-05-29 19:16:50 +0530

    Instruction fine-tune dataloader code commit.

---
+++ Dataloader/Instruction_finetuning/gpt2_instructDataFormat.py
@@ -0,0 +1,45 @@
+import torch
+
+def save_model_response(data, generate):
+    torch.manual_seed(123)
+
+    for i, row in enumerate(data):
+        _, input_text = format_input_response(row, inference=True)
+        model_output = generate.text_generation(input_text = input_text, max_new_tokens=256, temp=0.0,top_k= None, eos_id=50256)
+        model_response = model_output[len(input_text):].replace("### Response:", "").replace('Response:', '').strip()
+        data[i]['model_response'] = model_response
+
+
+    return data
+
+
+def format_input_response(input_json, prompt_style = 'alpaca', inference=False):
+
+    if prompt_style == 'alpaca':
+        instruction = (f"Below is an instruction that describes a task. "
+                       f"Write a response that appropriately completes the request."
+                       f"\n\n### Instruction:\n {input_json['instruction']}"
+                    )
+        input = f"\n\n### Input:\n {input_json['input']}" if input_json['input'] else ""
+
+        response = f"\n\n### Response:\n{input_json['output']}\n"
+
+        formatted_input_with_response = instruction + input + response
+        instruction_length = len(instruction + input)
+        inf_format = instruction + input 
+
+    else:
+
+        instruction = f"\n\n<|user|>\n {input_json['instruction']} "
+                            
+        input = f": {input_json['input']}" if input_json['input'] else ""
+
+        response =f"\n\n<|assistant|>\n {input_json['output']}"
+                    
+
+        formatted_input_with_response = instruction + input + response
+        instruction_length = len(instruction + input)
+        inf_format = instruction + input 
+
+    return instruction_length, inf_format if inference else formatted_input_with_response 
+
