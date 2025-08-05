0b16b91df9f663e284ee783b74a226a9ee279471
NamrataThakur <namrata.thakur893@gmail.com>
2025-08-05 09:07:31 +0530

    Updated Bash Fle with Lora, PFT, IFT command.

--- run_gpt2_train.sh
+++ run_gpt2_train.sh
@@ -16,9 +16,9 @@ val_split=0.05
 train_split=0.85
 batch_size=8
 seed=123
-pre_save_model=None
+pre_save_model="gpt2_355M_MaskedInstruct_FineTuned_v1.pth"
 load_weights=True
-peft_type=None
+peft_type="lora"
 mask_instruction=True
 dropout_rate=0.1
 eos_id=50256
@@ -27,13 +27,15 @@ use_gradient_clip=True
 warmup_steps=20
 initial_lr=1e-5
 min_lr=1e-5
+lora_rank=16
+lora_alpha=16
 python gpt_trainingPipeline.py \
-  --experiment_name 'IFT_Exp_MaskedInst_v1' \
+  --experiment_name 'IFT_Exp_MaskedInst_v1_LoRA' \
   --base_modelName 'gpt2_355M' \
   --data_path 'instruction-data-NT.json' \
   --training_type 'IFT' \
   --load_weights $load_weights \
-  --model_name 'gpt2_355M_MaskedInstruct_FineTuned_v1' \
+  --model_name 'gpt2_355M_MaskedInstruct_FineTuned_v1_LoRA' \
   --tokenizer 'tiktoken' \
   --seed $seed \
   --batch_size $batch_size \
@@ -41,7 +43,9 @@ python gpt_trainingPipeline.py \
   --val_split $val_split \
   --context_length $context_length \
   --max_new_tokens $max_new_tokens \
+  --pre_save_model $pre_save_model \
   --temp $temp \
+  --peft_type $peft_type \
   --dropout_rate $dropout_rate \
   --num_epochs $num_epochs \
   --max_training_length 'longest_training_example' \
@@ -53,7 +57,9 @@ python gpt_trainingPipeline.py \
   --use_gradient_clip $use_gradient_clip \
   --warmup_steps $warmup_steps \
   --initial_lr $initial_lr \
-  --min_lr $min_lr
+  --min_lr $min_lr \
+  --lora_rank $lora_rank \
+  --lora_alpha $lora_alpha 
 
 
 # Classification Supervised Fine-Tune (SFT) params:
@@ -69,9 +75,9 @@ python gpt_trainingPipeline.py \
 # train_split=0.7
 # batch_size=8
 # seed=123
-# pre_save_model=None
+# pre_save_model="gpt2_124M_SFT_Spam_v2.pth"
 # load_weights=True
-# peft_type=None
+# peft_type="lora"
 # mask_instruction=False
 # dropout_rate=0.0
 # eos_id=50256
@@ -80,16 +86,20 @@ python gpt_trainingPipeline.py \
 # warmup_steps=20
 # initial_lr=1e-5
 # min_lr=1e-5
+# lora_rank=16
+# lora_alpha=16
 # python gpt_trainingPipeline.py \
-#   --experiment_name 'SFT_Exp_ALL_v2' \
+#   --experiment_name 'SFT_Exp_ALL_v2_LoRA_noGC' \
 #   --base_modelName 'gpt2_124M' \
 #   --data_path 'sms_spam_collection.zip' \
 #   --training_type 'SFT' \
 #   --load_weights $load_weights \
-#   --model_name 'gpt2_124M_SFT_Spam_v2' \
+#   --model_name 'gpt2_124M_SFT_Spam_v2_LoRA_noGC' \
 #   --tokenizer 'tiktoken' \
 #   --seed $seed \
 #   --batch_size $batch_size \
+#   --pre_save_model $pre_save_model \
+#   --peft_type $peft_type \
 #   --train_split $train_split \
 #   --val_split $val_split \
 #   --context_length $context_length \
@@ -100,10 +110,63 @@ python gpt_trainingPipeline.py \
 #   --num_epochs $num_epochs \
 #   --max_training_length 'longest_training_example' \
 #   --top_k $top_k \
-#   --use_warmup $use_warmup \
-#   --use_gradient_clip $use_gradient_clip \
+#   --eos_id $eos_id  \
 #   --warmup_steps $warmup_steps \
 #   --initial_lr $initial_lr \
 #   --min_lr $min_lr \
+#   --lora_rank $lora_rank \
+#   --lora_alpha $lora_alpha \
 
 
+#Preference Fine-Tune (PFT) params:
+
+# ignore_index=-100
+# num_epochs=1
+# trainable_layers=None
+# #top_k=5
+# temp=0.0
+# max_new_tokens=40
+# context_length=1024
+# val_split=0.05
+# train_split=0.85
+# batch_size=8
+# seed=123
+# pre_save_model='gpt2_355M_MaskedInstruct_FineTuned_v1.pth'
+# load_weights=True
+# peft_type=None
+# mask_instruction=True
+# dropout_rate=0.0
+# eos_id=50256
+# use_warmup=False
+# use_gradient_clip=False
+# warmup_steps=20
+# initial_lr=1e-5
+# min_lr=1e-5
+# beta=0.1
+# python gpt_trainingPipeline.py \
+#   --experiment_name 'PFT_Exp_MaskedInst_v2' \
+#   --base_modelName 'gpt2_355M' \
+#   --data_path 'instruction-data-with-preference.json' \
+#   --training_type 'PFT' \
+#   --load_weights $load_weights \
+#   --model_name 'gpt2_355M_MaskedInstruct_PFT_v2' \
+#   --tokenizer 'tiktoken' \
+#   --pre_save_model $pre_save_model \
+#   --seed $seed \
+#   --batch_size $batch_size \
+#   --train_split $train_split \
+#   --val_split $val_split \
+#   --context_length $context_length \
+#   --max_new_tokens $max_new_tokens \
+#   --temp $temp \
+#   --dropout_rate $dropout_rate \
+#   --num_epochs $num_epochs \
+#   --max_training_length 'model_context_length' \
+#   --eos_id $eos_id \
+#   --prompt_style 'alpaca' \
+#   --ignore_index $ignore_index \
+#   --mask_instruction $mask_instruction \
+#   --warmup_steps $warmup_steps \
+#   --initial_lr $initial_lr \
+#   --min_lr $min_lr \
+#   --beta $beta
