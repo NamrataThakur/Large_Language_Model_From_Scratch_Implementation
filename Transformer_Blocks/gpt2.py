28d30cd7a232dc6387f08b8ff46284bdebc9454c
NamrataThakur <namrata.thakur893@gmail.com>
2025-08-06 07:07:05 +0530

    Added from_pretrained Class Function.

--- Transformer_Blocks/gpt2.py
+++ Transformer_Blocks/gpt2.py
@@ -2,8 +2,19 @@ import torch
 import torch.nn as nn
 from .transformer import TransformerBlock
 from .layernorm import LayerNormalization
+from huggingface_hub import PyTorchModelHubMixin
+import os
+import configparser
+from parameter_efficient_training.apply_lora import *
+from gpt_ClassificationFT.gpt2_model_config import GPT2_ModelConfig
+import requests
 
-class GPT2(nn.Module):
+config = configparser.ConfigParser()
+config.read("config.ini")
+MODEL_ROOT_FOLDER = config['PATHS']['MODEL_ROOT_FOLDER']
+
+
+class GPT2(nn.Module, PyTorchModelHubMixin):
     def __init__(self, config):
         super().__init__()
         self.token_embedding = nn.Embedding(config['vocab_size'],config['embedding_dimension'])
@@ -44,6 +55,80 @@ class GPT2(nn.Module):
         logits = self.final_projection(input)
 
         return logits
+    
+    @classmethod
+    def from_pretrained(self, base_modelName, model_name, device='cuda', num_classes = None, classify = False, lora = False):
+
+        try:
+           
+            device = torch.device(device)
+
+            m_config = GPT2_ModelConfig()
+            model_path = os.path.join(MODEL_ROOT_FOLDER,model_name)
+            print('Model Path : ', model_path)
+
+            m_config = GPT2_ModelConfig()
+            gpt2_config = m_config.load_model_config(model_name=base_modelName)
+            gpt2_baseInst = GPT2(gpt2_config)
+
+            if os.path.exists(model_path):
+
+                if classify:
+                    print('*********************** Loading the Classification Supervised Fine-Tune Model ***********************')
+                    in_features = gpt2_config['embedding_dimension']
+                    out_features = num_classes
+
+                    #Add the classification layer:
+                    gpt2_baseInst.final_projection = torch.nn.Linear(in_features=in_features, out_features= out_features)
+
+                    if lora:
+                        params_orig = freeze_model(gpt2_baseInst)
+                        lora_parameterization(model=gpt2_baseInst, rank = 16, alpha = 16)
+                else:
+                    print('*********************** Loading the Preference Fine-Tune Model ***********************')
+
+                checkpoint = torch.load(model_path, map_location=device, weights_only=True)
+                print('Checkpoint :: ', checkpoint.keys())
+                gpt2_baseInst.load_state_dict(checkpoint['model'] )
+
+                gpt2_baseInst.eval()
+                gpt2_baseInst.to(device)
+
+                return gpt2_baseInst
+
+            else:
+                print('Model weights not found in local. Loading the weights from HuggingFace Hub..!')
+
+                if not os.path.exists(MODEL_ROOT_FOLDER):
+                    os.mkdir(MODEL_ROOT_FOLDER)
+                
+                if classify:
+                    url = "https://huggingface.co/NamrataThakur/GPT2_124M_SFT_Spam/raw/main/gpt2_124M_SFT_Spam_v2_LoRA_noGC.pth"
+
+                else:
+                    url = "https://huggingface.co/NamrataThakur/GPT2_124M_SFT_Spam/raw/main/gpt2_124M_SFT_Spam_v2_LoRA_noGC.pth"
+
+                with requests.get(url, stream=True) as r:
+                    r.raise_for_status()
+                    with open(model_path, 'wb') as f:
+                        for chunk in r.iter_content(chunk_size=8192):
+                            f.write(chunk)
+
+                self.from_pretrained(base_modelName,model_name, device,num_classes, classify, lora)
+
+
+        except Exception as e:
+
+            print("Exception while loading the model weights : ", e)
+
+
+
+
+
+
+
+
+
     
 
 
