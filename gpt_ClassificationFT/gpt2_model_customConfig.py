70e655a295374c9ba1bbe6a58622ee5f8724c987
NamrataThakur <namrata.thakur893@gmail.com>
2025-09-14 08:37:09 +0530

    Version 2: Custom GPT Config class commit.

---
+++ gpt_ClassificationFT/gpt2_model_customConfig.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass
+
+@dataclass
+class GPT2_CustomConfig:
+    def __init__(self, arg_config):
+        
+        self.config = {
+            'vocab_size' :  50257,
+            'embedding_dimension':arg_config['embedding_dimension'],
+            'num_heads':arg_config['num_heads'],
+            'context_length':arg_config['context_length'],
+            'dropout':arg_config['dropout'],
+            'qkv_bias':arg_config['qkv_bias'],
+            'num_layers':arg_config['num_layers'],
+            'rms_eps':arg_config['rms_eps']
+        }
+
+        self.optim_config = {
+            'weight_decay' : arg_config['weight_decay'],
+            'beta1' : arg_config['beta1'],
+            'beta2' : arg_config['beta2']
+        }
