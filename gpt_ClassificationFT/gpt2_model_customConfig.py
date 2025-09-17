62c06341a148a209ea643404089e081c05689a77
NamrataThakur <namrata.thakur893@gmail.com>
2025-09-17 08:35:11 +0530

    Version 2: Updated custom config, handson notebook commit.

--- gpt_ClassificationFT/gpt2_model_customConfig.py
+++ gpt_ClassificationFT/gpt2_model_customConfig.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass
 class GPT2_CustomConfig:
     def __init__(self, arg_config):
         
+        # Model Configs:
         self.config = {
             'vocab_size' :  50257,
             'embedding_dimension':arg_config['embedding_dimension'],
@@ -12,10 +13,19 @@ class GPT2_CustomConfig:
             'dropout':arg_config['dropout'],
             'qkv_bias':arg_config['qkv_bias'],
             'num_layers':arg_config['num_layers'],
+
+            # RMS Configs:
             'rms_eps':arg_config['rms_eps'],
+            'rms_bias':arg_config['rms_bias'],
+
+            # RoPE Config:
+            'theta_base':arg_config['theta_base'],
+
+            # Group Query Attention Config:
             'num_kv_groups': arg_config['num_kv_groups']
         }
-
+        
+        # Optimization Configs:
         self.optim_config = {
             'weight_decay' : arg_config['weight_decay'],
             'beta1' : arg_config['beta1'],
