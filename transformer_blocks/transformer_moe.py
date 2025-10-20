9e0cc4a779dd639cc8dbc842064ed94a4267d8ae
NamrataThakur <namrata.thakur893@gmail.com>
2025-10-20 13:10:15 +0530

    Version 2: Transformer block with MoE class code committed

---
+++ transformer_blocks/transformer_moe.py
@@ -0,0 +1,43 @@
+import torch
+import torch.nn as nn
+
+import os
+os.pardir
+
+from .rmsnorm import RMSNormalization
+from .moe import MixtureofExperts
+from attention_implementation.group_query_attention import GroupQueryAttention
+
+
+class MoETransformerBlock(nn.Module):
+    def __init__(self, config, noise=False):
+        super().__init__()
+
+        self.attention_block = GroupQueryAttention(config=config)
+        self.rms_norm_attention = RMSNormalization(config=config)
+        self.rms_norm_moe = RMSNormalization(config=config)
+        self.moe_block = MixtureofExperts(config=config, noise=noise)
+
+    
+    def forward(self, input_tensor, rope, cache = None):
+
+        #Attention Block:
+        residual_conn = input_tensor
+        input_tensor = self.rms_norm_attention(input_tensor)
+
+        #K_V Cache:
+        input_tensor, new_cache = self.attention_block(input_tensor, rope, cache)
+
+        # Shape: (batch, seq_length, embedding_dim) + (batch, seq_length, embedding_dim) --> (batch, seq_length, embedding_dim)
+        input_tensor = residual_conn + input_tensor
+
+        #FeedForward / MoE Block:
+        residual_conn = input_tensor
+        input_tensor = self.rms_norm_moe(input_tensor)
+        input_tensor = self.moe_block(input_tensor)
+
+        # Shape: (batch, seq_length, embedding_dim) + (batch, seq_length, embedding_dim) --> (batch, seq_length, embedding_dim)
+        input_tensor = residual_conn + input_tensor
+
+        return input_tensor, new_cache
+
