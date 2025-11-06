37cdae6f736f70528ee8cb3c30ad10804cca6158
NamrataThakur <namrata.thakur893@gmail.com>
2025-11-06 08:56:27 +0530

    Updating MoE class with noise

--- transformer_blocks/transformer_moe.py
+++ transformer_blocks/transformer_moe.py
@@ -10,23 +10,23 @@ from attention_implementation.group_query_attention import GroupQueryAttention
 
 
 class MoETransformerBlock(nn.Module):
-    def __init__(self, config, noise=False):
+    def __init__(self, config):
         super().__init__()
 
         self.attention_block = GroupQueryAttention(config=config)
         self.rms_norm_attention = RMSNormalization(config=config)
         self.rms_norm_moe = RMSNormalization(config=config)
-        self.moe_block = MixtureofExperts(config=config, noise=noise)
+        self.moe_block = MixtureofExperts(config=config)
 
     
-    def forward(self, input_tensor, rope, cache = None):
+    def forward(self, input_tensor, rope, mask, offset = 0, cache = None):
 
         #Attention Block:
         residual_conn = input_tensor
         input_tensor = self.rms_norm_attention(input_tensor)
 
         #K_V Cache:
-        input_tensor, new_cache = self.attention_block(input_tensor, rope, cache)
+        input_tensor, new_cache = self.attention_block(input_tensor, rope, mask=mask, offset=offset, cache=cache)
 
         # Shape: (batch, seq_length, embedding_dim) + (batch, seq_length, embedding_dim) --> (batch, seq_length, embedding_dim)
         input_tensor = residual_conn + input_tensor
